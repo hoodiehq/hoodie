bfcacbace714436df1e7410d4678bebd7e385e0d
Jan Lehnardt <jan@apache.org>
2013-01-21 14:06:03 +0100

    add test for admin routing

--- test/test.js
+++ test/test.js
@@ -13,6 +13,10 @@ describe("hoodie-server", function() {
       assert(hs.serve_cors("api.a.jit.su", "a"));
     });
 
+    it("should serve admin", function() {
+      assert(hs.serve_admin("admin.a.jit.su", "a"));
+    });
+
     it("should fail with app name mismatch", function() {
       assert.equal(false, hs.serve_cors("a.jit.su", "b"));
     });
