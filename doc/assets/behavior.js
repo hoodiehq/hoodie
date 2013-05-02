2745f85bb4a2ddb031977ecf4bb321ea584041fd
Gregor Martynus <gregor@martynus.net>
2013-05-02 23:55:34 +0200

    updated docs

--- doc/assets/behavior.js
+++ doc/assets/behavior.js
@@ -2451,6 +2451,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "type": "heading",
           "data": {
             "level": 2,
+            "title": "dispose",
+            "slug": "dispose"
+          },
+          "depth": 2
+        }, {
+          "type": "heading",
+          "data": {
+            "level": 2,
             "title": "Extending hoodie",
             "slug": "extending-hoodie"
           },
