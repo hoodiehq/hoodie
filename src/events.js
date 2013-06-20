1062bfd76b6ba1c468de3977d5c1026202b74d28
Sven Lito <me@svenlito.com>
2013-06-20 22:14:07 +0100

    converting bits, adding comments back

--- src/events.js
+++ src/events.js
@@ -1,12 +1,16 @@
 var Events;
 
-Events = (function () {
-
-  'use strict';
+Events = (function() {
 
   function Events() {}
 
-  Events.prototype.bind = function (ev, callback) {
+  // ## Bind
+  //
+  // bind a callback to an event triggerd by the object
+  //
+  //     object.bind 'cheat', blame
+  //
+  Events.prototype.bind = function(ev, callback) {
     var calls, evs, name, _i, _len, _results;
     evs = ev.split(' ');
     calls = this.hasOwnProperty('_callbacks') && this._callbacks || (this._callbacks = {});
@@ -19,58 +23,90 @@ Events = (function () {
     return _results;
   };
 
+  // alias
   Events.prototype.on = Events.prototype.bind;
 
-  Events.prototype.one = function (ev, callback) {
-    return this.bind(ev, function () {
-      this.unbind(ev, this.bind());
+  // ## one
+  //
+  // same as `bind`, but does get executed only once
+  //
+  //     object.one 'groundTouch', gameOver
+  //
+  Events.prototype.one = function(ev, callback) {
+    return this.bind(ev, function() {
+      this.unbind(ev, arguments.callee);
       return callback.apply(this, arguments);
     });
   };
 
-  Events.prototype.trigger = function () {
+  // ## trigger
+  //
+  // trigger an event and pass optional parameters for binding.
+  //     object.trigger 'win', score: 1230
+  //
+  Events.prototype.trigger = function() {
     var args, callback, ev, list, _i, _len, _ref;
+
     args = 1 <= arguments.length ? Array.prototype.slice.call(arguments, 0) : [];
     ev = args.shift();
-    list = this.hasOwnProperty('_callbacks') && ((_ref = this._callbacks) !== null ? _ref[ev] : void 0);
+    list = this.hasOwnProperty('_callbacks') && ((_ref = this._callbacks) !== null ? _ref[ev] : null);
+
     if (!list) {
       return;
     }
+
     for (_i = 0, _len = list.length; _i < _len; _i++) {
       callback = list[_i];
       callback.apply(this, args);
     }
+
     return true;
   };
 
-  Events.prototype.unbind = function (ev, callback) {
+  // ## unbind
+  //
+  // unbind to from all bindings, from all bindings of a specific event
+  // or from a specific binding.
+  //
+  //     object.unbind()
+  //     object.unbind 'move'
+  //     object.unbind 'move', follow
+  //
+  Events.prototype.unbind = function(ev, callback) {
     var cb, i, list, _i, _len, _ref;
+
     if (!ev) {
       this._callbacks = {};
       return this;
     }
-    list = (_ref = this._callbacks) !== null ? _ref[ev] : void 0;
+
+    list = (_ref = this._callbacks) !== null ? _ref[ev] : null;
+
     if (!list) {
       return this;
     }
+
     if (!callback) {
       delete this._callbacks[ev];
       return this;
     }
+
     for (i = _i = 0, _len = list.length; _i < _len; i = ++_i) {
       cb = list[i];
-      if (!(cb === callback)) {
+
+      if (cb !== callback) {
         continue;
       }
+
       list = list.slice();
       list.splice(i, 1);
       this._callbacks[ev] = list;
       break;
     }
+
     return this;
   };
 
   return Events;
 
 })();
-
