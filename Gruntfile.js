5b2fb6b01c716b8472d7b557b38a89a8824fe998
Sven Lito <me@svenlito.com>
2013-06-21 21:53:36 +0100

    [fix #77] fix grunt shell:test

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,7 +70,9 @@ module.exports = function(grunt) {
     shell: {
       test: {
         command: 'node node_modules/testem/testem.js ci',
-        stdout: true
+        options: {
+          stdout: true
+        }
       }
     }
 
