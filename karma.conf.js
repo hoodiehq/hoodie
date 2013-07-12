97488265bed603e566a52065796fb699ffb23f4c
Sven Lito <me@svenlito.com>
2013-07-12 09:48:47 +0100

    set logging and CI settings

--- karma.conf.js
+++ karma.conf.js
@@ -57,7 +57,7 @@ colors = true;
 
 // level of logging
 // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_DEBUG;
+logLevel = LOG_INFO;
 
 
 // enable / disable watching file and executing tests whenever any file changes
@@ -81,4 +81,4 @@ captureTimeout = 60000;
 
 // Continuous Integration mode
 // if true, it capture browsers, run tests and exit
-singleRun = false;
+singleRun = true;
