4b609d4ab82919ad3f455868045dca6dc77df557
Gregor Martynus <gregor@martynus.net>
2013-08-02 12:02:26 +0200

    removed debugger statement
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm

--- test/specs/hoodie/dispose.spec.js
+++ test/specs/hoodie/dispose.spec.js
@@ -2,7 +2,6 @@ describe('hoodie.dispose()', function() {
   beforeEach(function() {
     this.sandbox = sinon.sandbox.create();
     this.hoodie = new Mocks.Hoodie();
-    debugger
     hoodieDispose(this.hoodie)
     this.sandbox.spy(this.hoodie, 'trigger');
   });
