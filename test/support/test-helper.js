c551c40b8ac30abf61511b15a246b76008b49877
Sven Lito <me@svenlito.com>
2013-08-07 14:47:16 +0100

    global test helpers

---
+++ test/support/test-helper.js
@@ -0,0 +1,24 @@
+/*globals before:true, after:true*/
+
+module.exports = {
+  before: (function () {
+
+    before(function () { });
+
+  }()),
+  beforeEach: (function () {
+
+    beforeEach(function () {});
+
+  }()),
+  afterEach: (function () {
+
+    afterEach(function () {});
+
+  }()),
+  after: (function () {
+
+    after(function () {});
+
+  }())
+};
