a5655a1da9d40d233c66833a45ae5e287c96c515
Caolan McMahon <caolan@caolanmcmahon.com>
2013-10-21 16:26:51 +0100

    set config dbs to admin only on initial setup, closes #97 * * *
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm

---
+++ test/test-config-dbs.js
@@ -0,0 +1,83 @@
+var couchr = require('couchr'),
+    async = require('async'),
+    environment = require('../lib/environment'),
+    config = require('../lib/config'),
+    app = require('../lib/app'),
+    rimraf = require('rimraf'),
+    path = require('path'),
+    url = require('url'),
+    _ = require('underscore');
+
+
+exports['check config dbs are private to admin'] = function (test) {
+  var project_dir = path.resolve(__dirname, 'fixtures/project1');
+
+  var cfg = environment.getConfig(
+    process.platform,   // platform
+    process.env,        // environment vars
+    project_dir,        // project directory
+    []                  // command-line arguments
+  );
+
+  cfg.admin_password = 'testing';
+
+  rimraf(path.resolve(project_dir, 'data'), function (err) {
+      if (err) {
+          return test.done(err);
+      }
+      app.init(cfg, function (err) {
+        if (err) {
+          return test.done(err);
+        }
+        async.parallel([
+            function (cb) {
+                var appdb = url.resolve(cfg.couch.url, '/app');
+                couchr.get(appdb, function (err, data, res) {
+                    test.equal(res.statusCode, 401);
+                    cb();
+                });
+            },
+            function (cb) {
+                var plugindb = url.resolve(cfg.couch.url, '/plugins');
+                couchr.get(plugindb, function (err, data, res) {
+                    test.equal(res.statusCode, 401);
+                    cb();
+                });
+            },
+            function (cb) {
+                var appdb = url.resolve(cfg.couch.url, '/app');
+                config.getCouchCredentials(cfg, function (err, username, password) {
+                    var parsed = url.parse(appdb);
+                    parsed.auth = username + ':' + password;
+                    appdb = url.format(parsed);
+                    couchr.get(appdb, function (err, data, res) {
+                        test.equal(res.statusCode, 200);
+                        cb();
+                    });
+                });
+            },
+            function (cb) {
+                var plugindb = url.resolve(cfg.couch.url, '/plugins');
+                config.getCouchCredentials(cfg, function (err, username, password) {
+                    if (err) {
+                        return test.done(err);
+                    }
+                    var parsed = url.parse(plugindb);
+                    parsed.auth = username + ':' + password;
+                    plugindb = url.format(parsed);
+                    couchr.get(plugindb, function (err, data, res) {
+                        test.equal(res.statusCode, 200);
+                        cb();
+                    });
+                });
+            }
+        ],
+        function (err) {
+            if (err) {
+                return test.done(err);
+            }
+            test.done();
+        });
+      });
+  });
+};
