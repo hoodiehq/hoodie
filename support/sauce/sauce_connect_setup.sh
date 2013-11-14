84b767ec79e97626f4c1062c00b3dd19597505e7
Sven Lito <me@svenlito.com>
2013-11-14 14:16:41 +0100

    [wip]

--- support/sauce/sauce_connect_setup.sh
+++ support/sauce/sauce_connect_setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 # Setup and start Sauce Connect for your TravisCI build
 # This script requires your .travis.yml to include the following two private env variables:
 # SAUCE_USERNAME
@@ -15,35 +13,19 @@ set -e
 CONNECT_URL="http://saucelabs.com/downloads/Sauce-Connect-latest.zip"
 CONNECT_DIR="/tmp/sauce-connect-$RANDOM"
 CONNECT_DOWNLOAD="Sauce_Connect.zip"
-
-CONNECT_LOG="$LOGS_DIR/sauce-connect"
-CONNECT_STDOUT="$LOGS_DIR/sauce-connect.stdout"
-CONNECT_STDERR="$LOGS_DIR/sauce-connect.stderr"
+READY_FILE="connect-ready-$RANDOM"
 
 # Get Connect and start it
 mkdir -p $CONNECT_DIR
 cd $CONNECT_DIR
-curl $CONNECT_URL -o $CONNECT_DOWNLOAD 2> /dev/null 1> /dev/null
-unzip $CONNECT_DOWNLOAD > /dev/null
+curl $CONNECT_URL > $CONNECT_DOWNLOAD
+unzip $CONNECT_DOWNLOAD
 rm $CONNECT_DOWNLOAD
+java -jar Sauce-Connect.jar --readyfile $READY_FILE \
+    --tunnel-identifier $TRAVIS_JOB_NUMBER \
+    $SAUCE_USERNAME $SAUCE_ACCESS_KEY &
 
-SAUCE_ACCESS_KEY=`echo $SAUCE_ACCESS_KEY | rev`
-
-
-ARGS=""
-
-# Set tunnel-id only on Travis, to make local testing easier.
-if [ ! -z "$TRAVIS_JOB_NUMBER" ]; then
-  ARGS="$ARGS --tunnel-identifier $TRAVIS_JOB_NUMBER"
-fi
-if [ ! -z "$SAUCE_CONNECT_READY_FILE" ]; then
-  ARGS="$ARGS --readyfile $SAUCE_CONNECT_READY_FILE"
-fi
-
-
-echo "Starting Sauce Connect in the background, logging into:"
-echo "  $CONNECT_LOG"
-echo "  $CONNECT_STDOUT"
-echo "  $CONNECT_STDERR"
-java -jar Sauce-Connect.jar $ARGS $SAUCE_USERNAME $SAUCE_ACCESS_KEY \
-  --logfile $CONNECT_LOG 2> $CONNECT_STDERR 1> $CONNECT_STDOUT &
+# Wait for Connect to be ready before exiting
+while [ ! -f $READY_FILE ]; do
+  sleep .5
+done
