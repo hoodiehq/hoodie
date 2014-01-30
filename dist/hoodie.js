e0cf6cc251a75c2919f565165594511d5857fb4e
Gregor Martynus <gregor@martynus.net>
2014-01-30 13:33:39 +0100

    build
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm

--- dist/hoodie.js
+++ dist/hoodie.js
@@ -1,8 +1,3 @@
-// Hoodie.js - 0.5.4
-// https://github.com/hoodiehq/hoodie.js
-// Copyright 2012 - 2014 https://github.com/hoodiehq/
-// Licensed Apache License 2.0
-
 !function(e){"object"==typeof exports?module.exports=e():"function"==typeof define&&define.amd?define(e):"undefined"!=typeof window?window.Hoodie=e():"undefined"!=typeof global?global.Hoodie=e():"undefined"!=typeof self&&(self.Hoodie=e())}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var hasOwn = Object.prototype.hasOwnProperty;
 var toString = Object.prototype.toString;
