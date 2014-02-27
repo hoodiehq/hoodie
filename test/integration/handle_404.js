97813b939620bbe8c6b4fc6a2d8aa4532967f474
Lupo Montero <lupo@e-noise.com>
2014-02-27 00:34:31 -0500

    Serve index.html on 404 when accept text/html. Fixes #205

--- test/integration/handle_404.js
+++ test/integration/handle_404.js
@@ -9,64 +9,52 @@ var config = {
   admin_password: '12345'
 };
 
-var hasServer = false;
-function start_server(callback) {
-  if(hasServer) {
-    return callback();
-  }
-
-  hoodie_server.start(config, function() {
-    hasServer = true;
-    return callback();
-  });
-}
-
 describe('handle 404', function () {
+
+  before(function (done) {
+    hoodie_server.start(config, done);
+  });
+
+  // TODO: I guess we should kill the server once we are done with the tests
+  //after(function (done) {});
+
   it('should send index.html on accept: text/html', function (done) {
-
-    start_server(function() {
-
-      http.get({
-        host: '127.0.0.1',
-        port: config.www_port,
-        method: 'get',
-        path: '/does_not_exist',
-        headers: {
-          accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
-        }
-      }, function(res) {
-        var buf = '';
-        res.on('data', function(chunk) { buf += chunk});
-        res.on('end', function() {
-          expect(buf).to.be('hi' + os.EOL);
-          done();
-        });
+    http.get({
+      host: '127.0.0.1',
+      port: config.www_port,
+      method: 'get',
+      path: '/does_not_exist',
+      headers: {
+        accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+      }
+    }, function (res) {
+      var buf = '';
+      res.on('data', function (chunk) { buf += chunk});
+      res.on('end', function () {
+        expect(buf).to.be('hi' + os.EOL);
+        done();
       });
     });
   });
 
 
   it('should send a JSON 404 on anything but accept: text/html*', function (done) {
-
-    start_server(function() {
-
-      http.get({
-        host: '127.0.0.1',
-        port: config.www_port,
-        method: 'get',
-        path: '/does_not_exist',
-        headers: {
-          accept: 'application/json'
-        }
-      }, function(res) {
-        var buf = '';
-        res.on('data', function(chunk) { buf += chunk});
-        res.on('end', function() {
-          buf = JSON.parse(buf);
-          expect(buf.statusCode).to.be(404);
-          expect(buf.error).to.be('Not Found');
-          done();
-        });
+    http.get({
+      host: '127.0.0.1',
+      port: config.www_port,
+      method: 'get',
+      path: '/does_not_exist',
+      headers: {
+        accept: 'application/json'
+      }
+    }, function (res) {
+      var buf = '';
+      res.on('data', function (chunk) { buf += chunk});
+      res.on('end', function () {
+        buf = JSON.parse(buf);
+        expect(buf.statusCode).to.be(404);
+        expect(buf.error).to.be('Not Found');
+        done();
       });
     });
   });
