b3db1979703e8482e1c3726152f3a38d8eaeac63
Gregor Martynus <gregor@martynus.net>
2014-03-06 18:14:37 +0100

    config.js suggested changes @svnlto
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm

--- src/hoodie/config.js
+++ src/hoodie/config.js
@@ -7,8 +7,7 @@ var localstorage = require('../utils').localstorage;
 //
 function hoodieConfig(hoodie) {
 
-  var type = '$config';
-  var id = 'hoodie';
+  var CONFIG_STORE_KEY = '_hoodie_config';
   var cache = {};
 
   // public API
@@ -21,27 +20,10 @@ function hoodieConfig(hoodie) {
   // adds a configuration
   //
   config.set = function set(key, value) {
-    var isSilent, update;
-
-    if (cache[key] === value) {
-      return;
-    }
+    var update;
 
     cache[key] = value;
-
-    update = {};
-    update[key] = value;
-    isSilent = key.charAt(0) === '_';
-
-    // we have to assure that _hoodieId has always the
-    // same value as createdBy for $config/hoodie
-    // Also see config.js:77ff
-    if (key === '_hoodieId') {
-      localstorage.removeItem(type);
-      update = cache;
-    }
-
-    localstorage.setItem(type, id);
+    localstorage.setObject(CONFIG_STORE_KEY, cache);
   };
 
   // get
@@ -60,7 +42,7 @@ function hoodieConfig(hoodie) {
   //
   config.clear = function clear() {
     cache = {};
-    return localstorage.removeItem(type);
+    return localstorage.removeItem(CONFIG_STORE_KEY);
   };
 
   // unset
@@ -70,11 +52,8 @@ function hoodieConfig(hoodie) {
   // config.set(key, undefined).
   //
   config.unset = function unset(key) {
-    if (typeof config.get(key) === 'undefined') {
-      return resolve();
-    }
-
-    return config.set(key, undefined);
+    delete cache[key];
+    localstorage.setObject(CONFIG_STORE_KEY, cache);
   };
 
   //
@@ -82,14 +61,7 @@ function hoodieConfig(hoodie) {
   // The init method to be called on hoodie startup
   //
   function init() {
-    // TODO: I really don't like this being here. And I don't like that if the
-    //       store API will be truly async one day, this will fall on our feet.
-    //       We should discuss if we make config a simple object in localStorage,
-    //       outside of hoodie.store, and use localStorage sync API directly to
-    //       interact with it, also in future versions.
-    hoodie.store.find(type, id).done(function(obj) {
-      cache = obj;
-    });
+    cache = localstorage.getObject(CONFIG_STORE_KEY);
   }
 
   // allow to run init only once
