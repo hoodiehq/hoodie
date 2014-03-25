1eda7476be97b08c6be2ab7745d9e94385887af7
Gregor Martynus <gregor@martynus.net>
2014-03-25 16:59:03 +0100

    Release 0.7.4
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm

--- dist/hoodie.js
+++ dist/hoodie.js
@@ -389,6 +389,8 @@ function hoodieAccount(hoodie) {
   // to sign in with a 300ms timeout.
   //
   account.signUp = function signUp(username, password) {
+    var options;
+
 
     if (password === undefined) {
       password = '';
@@ -409,7 +411,7 @@ function hoodieAccount(hoodie) {
     // downcase username
     username = username.toLowerCase();
 
-    var options = {
+    options = {
       data: JSON.stringify({
         _id: userDocKey(username),
         name: userTypeAndId(username),
@@ -425,8 +427,8 @@ function hoodieAccount(hoodie) {
       contentType: 'application/json'
     };
 
-    return account.request('PUT', userDocUrl(username), options).then(
-    handleSignUpSuccess(username, password), handleSignUpError(username));
+    return account.request('PUT', userDocUrl(username), options)
+    .then(handleSignUpSuccess(username, password), handleSignUpError(username));
   };
 
 
@@ -831,7 +833,7 @@ function hoodieAccount(hoodie) {
     return function(response) {
       account.trigger('signup', username);
       userDoc._rev = response.rev;
-      return delayedSignIn(username, password);
+      return delayedSignIn(username, password, {moveData: true});
     };
   }
 
@@ -872,7 +874,7 @@ function hoodieAccount(hoodie) {
     }
 
     global.setTimeout(function() {
-      var promise = sendSignInRequest(username, password);
+      var promise = sendSignInRequest(username, password, options);
       promise.done(defer.resolve);
       promise.fail(function(error) {
         if (error.name === 'HoodieAccountUnconfirmedError') {
@@ -1314,7 +1316,7 @@ function hoodieAccount(hoodie) {
           // work around trouble in case of local changes. See 
           // https://github.com/hoodiehq/hoodie.js/issues/256
           return account.signOut({silent:true, ignoreLocalChanges: true}).then(function() {
-            return account.signIn(newUsername, newPassword);
+            return account.signIn(newUsername, newPassword, {moveData: true});
           });
         });
       } else {
