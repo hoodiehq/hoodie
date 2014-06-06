3d175dff660b132bf1cb1a5f8600fc1c8f31b774
Jean Carlos Meninno <eth0.lo@gmail.com>
2014-06-06 16:52:50 +0200

    Using bluebird to substite jQuery.Deferred

--- src/utils/promise/defer.js
+++ src/utils/promise/defer.js
@@ -1,1 +1,22 @@
-module.exports = global.jQuery.Deferred;
+var Promise = require('bluebird');
+
+module.exports = function Defer () {
+  var defer = this;
+
+  // Backwards compatibility with jQuery Defer
+  if (!(defer instanceof Defer)) {
+    return new Defer();
+  }
+
+  var resolve, reject;
+  var promise = new Promise(function(res, rej) {
+      resolve = res;
+      reject = rej;
+    });
+
+  return {
+      resolve: resolve,
+      reject: reject,
+      promise: promise
+    };
+};
