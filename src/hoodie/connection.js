417203355b4f982c5996cfac06ebd5fb8fcaf789
Stephan Bönnemann <stephan@boennemann.me>
2014-07-23 16:06:30 +0200

    refactor(connection): externalize state

--- src/hoodie/connection.js
+++ src/hoodie/connection.js
@@ -1,97 +1,79 @@
 // hoodie.checkConnection() & hoodie.isConnected()
 // =================================================
 
-
-var reject = require('../utils/promise/reject');
-var resolve = require('../utils/promise/resolve');
+var promise = require('../utils/promise');
 
 //
-function hoodieConnection(hoodie) {
-
+var exports = module.exports = function(hoodie) {
   // state
-  var online = true;
-  var checkConnectionInterval = 30000;
-  var checkConnectionRequest = null;
-  var checkConnectionTimeout = null;
-
-  // Check Connection
-  // ------------------
-
-  // the `checkConnection` method is used, well, to check if
-  // the hoodie backend is reachable at `baseUrl` or not.
-  // Check Connection is automatically called on startup
-  // and then each 30 seconds. If it fails, it
-  //
-  // - sets `online = false`
-  // - triggers `offline` event
-  // - sets `checkConnectionInterval = 3000`
-  //
-  // when connection can be reestablished, it
-  //
-  // - sets `online = true`
-  // - triggers `online` event
-  // - sets `checkConnectionInterval = 30000`
-  //
-  hoodie.checkConnection = function checkConnection() {
-    var req = checkConnectionRequest;
-    var path = '/?hoodieId=' + hoodie.id();
-
-    if (req && req.state() === 'pending') {
-      return req;
-    }
-
-    global.clearTimeout(checkConnectionTimeout);
-
-    checkConnectionRequest = hoodie.request('GET', path).then(
-      handleCheckConnectionSuccess,
-      handleCheckConnectionError
-    );
-
-    return checkConnectionRequest;
+  var state = {
+    online: true,
+    checkConnectionRequest: null,
+    checkConnectionTimeout: null,
+    hoodie: hoodie
   };
 
+  hoodie.checkConnection = exports.checkConnection.bind(null, state);
+  hoodie.isConnected = exports.isConnected.bind(null, state);
+};
 
-  // isConnected
-  // -------------
+// Check Connection
+// ------------------
 
-  //
-  hoodie.isConnected = function isConnected() {
-    return online;
-  };
+// the `checkConnection` method is used, well, to check if
+// the hoodie backend is reachable at `baseUrl` or not.
+// Check Connection is automatically called on startup
+// and then each 30 seconds. If it fails, it
+//
+// - sets `online = false`
+// - triggers `disconnected` event
+// - checks again in 3 seconds
+//
+// when connection can be reestablished, it
+//
+// - sets `online = true`
+// - triggers `reconnected` event
+//
+exports.checkConnection = function(state) {
+  var req = state.checkConnectionRequest;
+  var path = '/?hoodieId=' + state.hoodie.id();
 
-
-  //
-  //
-  //
-  function handleCheckConnectionSuccess() {
-    checkConnectionInterval = 30000;
-
-    checkConnectionTimeout = global.setTimeout(hoodie.checkConnection, checkConnectionInterval);
-
-    if (!hoodie.isConnected()) {
-      hoodie.trigger('reconnected');
-      online = true;
-    }
-
-    return resolve();
+  if (req && req.state() === 'pending') {
+    return req;
   }
 
+  global.clearTimeout(state.checkConnectionTimeout);
 
-  //
-  //
-  //
-  function handleCheckConnectionError() {
-    checkConnectionInterval = 3000;
+  state.checkConnectionRequest = state.hoodie.request('GET', path).then(
+    exports.handleConnection.bind(null, state, 30000, 'reconnected', true),
+    exports.handleConnection.bind(null, state, 3000, 'disconnected', false)
+  );
 
-    checkConnectionTimeout = global.setTimeout(hoodie.checkConnection, checkConnectionInterval);
+  return state.checkConnectionRequest;
+};
 
-    if (hoodie.isConnected()) {
-      hoodie.trigger('disconnected');
-      online = false;
-    }
+// isConnected
+// -------------
 
-    return reject();
+//
+exports.isConnected = function(state) {
+  return state.online;
+};
+
+//
+//
+//
+exports.handleConnection = function(state, interval, event, online) {
+
+  state.checkConnectionTimeout = global.setTimeout(
+    exports.checkConnection.bind(null, state),
+    interval
+  );
+
+  if (exports.isConnected(state) !== online) {
+    state.hoodie.trigger(event);
+    state.online = online;
   }
-}
 
-module.exports = hoodieConnection;
+  return promise[online ? 'resolve' : 'reject']();
+};
