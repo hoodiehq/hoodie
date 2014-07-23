247a1ce61f8f53dd7e402530278adcf53939461d
Sven Lito <me@svenlito.com>
2014-07-23 17:42:15 +0200

    tests(rename): rename config to config_store

--- test/unit/config-test.js
+++ test/unit/config-test.js
@@ -1,64 +1,64 @@
 var expect = require('expect.js');
-var config = require('../../lib/core/config');
+var configStore = require('../../lib/core/config_store');
 
 var _ = require('lodash');
 
-describe('config', function () {
+describe('configStore', function () {
 
   it('should expose n number of properties', function () {
-    expect(_.size(config)).to.eql(13);
+    expect(_.size(configStore)).to.eql(13);
   });
 
   it('should have a path property', function () {
-    expect(config).to.have.property('path');
+    expect(configStore).to.have.property('path');
   });
 
   it('should have a read property', function () {
-    expect(config).to.have.property('read');
+    expect(configStore).to.have.property('read');
   });
 
   it('should have a write property', function () {
-    expect(config).to.have.property('write');
+    expect(configStore).to.have.property('write');
   });
 
   it('should have a get property', function () {
-    expect(config).to.have.property('get');
+    expect(configStore).to.have.property('get');
   });
 
   it('should have a set property', function () {
-    expect(config).to.have.property('set');
+    expect(configStore).to.have.property('set');
   });
 
   it('should have a getCouchPassword property', function () {
-    expect(config).to.have.property('getCouchPassword');
+    expect(configStore).to.have.property('getCouchPassword');
   });
 
   it('should have a getCouchUsername property', function () {
-    expect(config).to.have.property('getCouchUsername');
+    expect(configStore).to.have.property('getCouchUsername');
   });
 
   it('should have a getCouchCredentials property', function () {
-    expect(config).to.have.property('getCouchCredentials');
+    expect(configStore).to.have.property('getCouchCredentials');
   });
 
   it('should have a setCouchPassword property', function () {
-    expect(config).to.have.property('setCouchPassword');
+    expect(configStore).to.have.property('setCouchPassword');
   });
 
   it('should have a setCouchUsername property', function () {
-    expect(config).to.have.property('setCouchUsername');
+    expect(configStore).to.have.property('setCouchUsername');
   });
 
   it('should have a setCouchCredentials property', function () {
-    expect(config).to.have.property('setCouchCredentials');
+    expect(configStore).to.have.property('setCouchCredentials');
   });
 
   it('should have a setProperty property', function () {
-    expect(config).to.have.property('setProperty');
+    expect(configStore).to.have.property('setProperty');
   });
 
   it('should have a getProperty property', function () {
-    expect(config).to.have.property('getProperty');
+    expect(configStore).to.have.property('getProperty');
   });
 
 });
