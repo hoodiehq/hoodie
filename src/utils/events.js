b1b5bd9f67d09d2eeb2e26951dab39de026199f6
Stephan Bönnemann <stephan@boennemann.me>
2014-07-24 18:39:43 +0200

    refactor(events): replace spline events w/ EventEmitter

---
+++ src/utils/events.js
@@ -0,0 +1,60 @@
+/*jshint forin: false */
+var EventEmitter = require('events').EventEmitter;
+var inherits = require('inherits');
+var extend = require('extend');
+
+
+var exports = module.exports = function(hoodie) {
+  var emitter = new exports.HoodieEventEmitter();
+  extend(hoodie, emitter);
+  return emitter;
+};
+
+exports.MAP = {
+  bind: 'on',
+  one: 'once',
+  trigger: 'emit',
+  unbind: 'removeListener',
+};
+
+exports.METHODS = (function() {
+  var METHODS = [];
+  var pt = EventEmitter.prototype;
+
+  for (var fn in pt) {
+    if (typeof pt[fn] === 'function') {
+      METHODS.push(fn);
+    }
+  }
+
+  return METHODS.concat(Object.keys(exports.MAP));
+})();
+
+exports.HoodieEventEmitter = (function() {
+  function HoodieEventEmitter() {
+    EventEmitter.call(this);
+  }
+
+  inherits(HoodieEventEmitter, EventEmitter);
+
+  Object.keys(exports.MAP).forEach(function(fn) {
+    HoodieEventEmitter.prototype[fn] = EventEmitter.prototype[exports.MAP[fn]];
+  });
+
+  return HoodieEventEmitter;
+})();
+
+exports.scopedEventEmitter = function(hoodie, context, namespace) {
+  var scopedEmitter = {};
+
+  exports.METHODS.forEach(function(fn) {
+    scopedEmitter[fn] = function() {
+      var args = [].slice.call(arguments);
+      args[0] = namespace + ':' + args[0];
+      hoodie[fn].apply(hoodie, args);
+    };
+  });
+
+  extend(context, scopedEmitter);
+  return scopedEmitter;
+};
