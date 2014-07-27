3a131ec23014e8faa27d5ed824d53e4520dd6762
Stephan Bönnemann <stephan@boennemann.me>
2014-07-27 18:48:47 +0200

    refactor(hoodie): proper closure and dry code

--- src/hoodie.js
+++ src/hoodie.js
@@ -4,15 +4,6 @@
 // the door to world domination (apps)
 //
 
-var hoodieAccount = require('./hoodie/account');
-var hoodieAccountRemote = require('./hoodie/remote');
-var hoodieConnection = require('./hoodie/connection');
-var hoodieId = require('./hoodie/id');
-var hoodieLocalStore = require('./hoodie/store');
-var hoodieTask = require('./hoodie/task');
-var hoodieOpen = require('./hoodie/open');
-var hoodieRequest = require('./hoodie/request');
-
 // for plugins
 var lib = require('./lib');
 var utils = require('./utils');
@@ -27,92 +18,84 @@ var utils = require('./utils');
 //     // init a new hoodie instance
 //     hoodie = new Hoodie
 //
-var Hoodie = module.exports = function (baseUrl) {
-  var hoodie = this;
+module.exports = (function() {
+  function Hoodie(baseUrl) {
+    var hoodie = this;
 
-  // enforce initialization with `new`
-  if (!(hoodie instanceof Hoodie)) {
-    throw new Error('usage: new Hoodie(url);');
+    // enforce initialization with `new`
+    if (!(hoodie instanceof Hoodie)) {
+      throw new Error('usage: new Hoodie(url);');
+    }
+
+    // remove trailing slashes
+    hoodie.baseUrl = baseUrl ? baseUrl.replace(/\/+$/, '') : '';
+
+
+    // hoodie.extend
+    // ---------------
+
+    // extend hoodie instance:
+    //
+    //     hoodie.extend(function(hoodie) {} )
+    //
+    hoodie.extend = function extend(extension) {
+      extension(hoodie, lib, utils);
+    };
+
+    utils.events(hoodie);
+
+    //
+    // Extending hoodie core
+    //
+
+    // order matters b/c of cross module dependencies
+    [
+      'account',
+      'remote',
+      'connection',
+      'id',
+      'task',
+      'open',
+      'store',
+      'request'
+    ].forEach(function(module) {
+      hoodie.extend(require('./hoodie/' + module));
+    });
+
+    // authenticate
+    // we use a closure to not pass the username to connect, as it
+    // would set the name of the remote store, which is not the username.
+    hoodie.account.authenticate().then(function( /* username */ ) {
+      hoodie.remote.connect();
+    });
+
+    //
+    // loading user extensions
+    //
+    applyExtensions(hoodie);
   }
 
-  // remove trailing slashes
-  hoodie.baseUrl = baseUrl ? baseUrl.replace(/\/+$/, '') : '';
+  // Extending hoodie
+  // ------------------
 
-
-  // hoodie.extend
-  // ---------------
-
-  // extend hoodie instance:
+  // You can extend the Hoodie class like so:
   //
-  //     hoodie.extend(function(hoodie) {} )
+  // Hoodie.extend(function(hoodie) { hoodie.myMagic = function() {} })
   //
-  hoodie.extend = function extend(extension) {
-    extension(hoodie, lib, utils);
+  var extensions = [];
+
+  //
+  // detect available extensions and attach to Hoodie Object.
+  //
+  function applyExtensions(hoodie) {
+    for (var i = 0; i < extensions.length; i++) {
+      extensions[i](hoodie, lib, utils);
+    }
+  }
+
+  Hoodie.extend = function(extension) {
+    extensions.push(extension);
   };
 
-  utils.events(hoodie);
-
-  //
-  // Extending hoodie core
-  //
-
-  // * hoodie.isOnline
-  // * hoodie.checkConnection
-  hoodie.extend(hoodieConnection);
-
-  // * hoodie.open
-  hoodie.extend(hoodieOpen);
-
-  // * hoodie.store
-  hoodie.extend(hoodieLocalStore);
-
-  // * hoodie.task
-  hoodie.extend(hoodieTask);
-
-  // * hoodie.account
-  hoodie.extend(hoodieAccount);
-
-  // * hoodie.remote
-  hoodie.extend(hoodieAccountRemote);
-
-  // * hoodie.id
-  hoodie.extend(hoodieId);
-
-  // * hoodie.request
-  hoodie.extend(hoodieRequest);
-
-  // authenticate
-  // we use a closure to not pass the username to connect, as it
-  // would set the name of the remote store, which is not the username.
-  hoodie.account.authenticate().then(function( /* username */ ) {
-    hoodie.remote.connect();
-  });
-
-  //
-  // loading user extensions
-  //
-  applyExtensions(hoodie);
-};
-
-// Extending hoodie
-// ------------------
-
-// You can extend the Hoodie class like so:
-//
-// Hoodie.extend(function(hoodie) { hoodie.myMagic = function() {} })
-//
-var extensions = [];
-
-Hoodie.extend = function(extension) {
-  extensions.push(extension);
-};
-
-//
-// detect available extensions and attach to Hoodie Object.
-//
-function applyExtensions(hoodie) {
-  for (var i = 0; i < extensions.length; i++) {
-    extensions[i](hoodie, lib, utils);
-  }
-}
-
+  return Hoodie;
+})();
