31b029b782f0b382b2a4fe3df3613497a757f6d1
Stephan Bönnemann <stephan@boennemann.me>
2014-07-27 18:32:29 +0200

    refactor(store): externalize state in bootstrap

---
+++ src/hoodie/store/bootstrap.js
@@ -0,0 +1,42 @@
+var localStore = require('./localstore');
+
+var exports = module.exports;
+
+//
+exports.start = function(state) {
+  state.bootstrapping = true;
+  state.hoodie.store.emit('bootstrap:start');
+};
+
+//
+exports.end = function(state) {
+  var methodCall, method, args, defer;
+
+  state.bootstrapping = false;
+  while (state.queue.length > 0) {
+    methodCall = state.queue.shift();
+    method = methodCall[0];
+    args = methodCall[1];
+    defer = methodCall[2];
+    localStore[method]
+      .bind(localStore, state)
+      .apply(localStore, args)
+      .then(defer.resolve, defer.reject);
+  }
+
+  state.hoodie.store.emit('bootstrap:end');
+};
+
+//
+exports.abort = function(state, error) {
+  var methodCall, defer;
+
+  state.bootstrapping = false;
+  while (state.queue.length > 0) {
+    methodCall = state.queue.shift();
+    defer = methodCall[2];
+    defer.reject(error);
+  }
+
+  state.hoodie.store.emit('bootstrap:error', error);
+};
