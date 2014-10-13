13f04d9c8809b0f4e4d770343e3dbd706526be96
Gregor Martynus <gregor@martynus.net>
2014-10-13 18:37:29 +0200

    test: all existing unit tests passing
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    
    http://go.hood.ie/thehoodiefirm

--- karma.conf.js
+++ karma.conf.js
@@ -41,7 +41,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO, // LOG_DEBUG,
 
 
     // enable / disable watching file and executing tests whenever any file changes
@@ -67,21 +67,20 @@ module.exports = function(config) {
       files: [
         // 'test/specs/**/*.spec.js'
 
-        // 'test/specs/hoodie/account.spec.js',
+        'test/specs/hoodie/account.spec.js',
         'test/specs/hoodie/connection.spec.js',
         'test/specs/hoodie/id.spec.js',
-        // 'test/specs/hoodie/open.spec.js',
-        // 'test/specs/hoodie/remote.spec.js',
+        'test/specs/hoodie/open.spec.js',
+        'test/specs/hoodie/remote.spec.js',
         'test/specs/hoodie/request.spec.js',
-        // 'test/specs/hoodie/store.spec.js',
-        // 'test/specs/hoodie/task.spec.js',
-
+        'test/specs/hoodie/store.spec.js',
+        'test/specs/hoodie/task.spec.js',
 
         'test/specs/lib/error/error.spec.js',
-        // 'test/specs/lib/store/api.spec.js',
-        // 'test/specs/lib/store/remote.spec.js',
-        // 'test/specs/lib/store/scoped.spec.js',
-        // 'test/specs/lib/task/scoped.spec.js',
+        'test/specs/lib/store/api.spec.js',
+        'test/specs/lib/store/remote.spec.js',
+        'test/specs/lib/store/scoped.spec.js',
+        'test/specs/lib/task/scoped.spec.js',
 
         'test/specs/utils/config.spec.js',
         'test/specs/utils/events.spec.js',
