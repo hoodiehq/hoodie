ac368b4953f8ba6cf3bea84a4347ac238fbb437e
Gregor Martynus <gregor@martynus.net>
2014-11-15 15:57:56 +0100

    fix(promise): pass progress callbacks on `.then`ing
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    
    http://go.hood.ie/thehoodiefirm

--- src/utils/promise/defer.js
+++ src/utils/promise/defer.js
@@ -21,7 +21,6 @@ module.exports = function Defer() {
 
   // add done, fail, always, progress callbacks
   wrapPromise(defer.promise);
-
   defer.promise._progressCallbacks = [];
   defer.notify = function notify() {
     var args = Array.prototype.slice.call(arguments);
@@ -57,9 +56,28 @@ function wrapPromise (promise) {
     }
     return this;
   };
-  promise.then = function then (onResolve, onReject) {
-    promise = Promise.prototype.then.call(this, onResolve, onReject);
+  promise.then = function (onResolve, onReject) {
+    promise = Promise.prototype.then.call(this,
+      passProgressCallbacks(this, onResolve),
+      onReject);
     wrapPromise(promise);
+    promise._progressCallbacks = this._progressCallbacks;
     return promise;
   };
+
+  function passProgressCallbacks(promise, callback) {
+    return function() {
+      var newPromise;
+
+      if (! callback) {
+        return;
+      }
+
+      newPromise = callback.apply(promise, arguments);
+      if (newPromise && newPromise._progressCallbacks && promise._progressCallbacks) {
+        newPromise._progressCallbacks = newPromise._progressCallbacks.concat(promise._progressCallbacks);
+      }
+      return newPromise;
+    };
+  }
 }
