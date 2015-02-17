hoodiehq/hoodie

commit 7072d10caa47c465e26f2d326ef0ccf13025c734

fix: promise handling
* * *

This commit was sponsored by The Hoodie Firm.
You can hire The Hoodie Firm:

http://go.hood.ie/thehoodiefirm

src/hoodie/request.js
@@ -82,7 +82,9 @@ exports.request = function(hoodie, type, url, options) {
   // as well. See also http://bugs.jquery.com/ticket/14104
   jQueryPromise = global.jQuery.ajax(extend(defaults, options))
     .done(requestDefer.resolve)
-    .fail(requestDefer.reject);
+    .fail(function(xhr, error) {
+      requestDefer.reject(error);
+    });
   var pipedPromise = requestPromise.then(
     null,
     exports.handleRequestError.bind(null, hoodie)

src/utils/promise/defer.js
@@ -4,13 +4,13 @@ var Promise = require('./promise');
 module.exports = function Defer() {
   var defer = {};
   defer.promise = new Promise(function (resolveCallback, rejectCallback) {
-    defer.resolve = function resolve() {
+    defer.resolve = function resolve(what) {
       defer.notify = function noop () {};
-      resolveCallback.apply(null, arguments);
+      resolveCallback(what);
     };
-    defer.reject = function reject() {
+    defer.reject = function reject(what) {
       defer.notify = function noop () {};
-      rejectCallback.apply(null, arguments);
+      rejectCallback(what);
     };
   });
 

commit 8c6512c19d6bd87b9bdc60e6872a8e4884a9d0da

fix: fixes bug introduced in 7072d10. Upgrade from anonymous accounts fails
* * *

This commit was sponsored by The Hoodie Firm.
You can hire The Hoodie Firm:

http://go.hood.ie/thehoodiefirm

src/hoodie/request.js
@@ -82,9 +82,7 @@ exports.request = function(hoodie, type, url, options) {
   // as well. See also http://bugs.jquery.com/ticket/14104
   jQueryPromise = global.jQuery.ajax(extend(defaults, options))
     .done(requestDefer.resolve)
-    .fail(function(xhr, error) {
-      requestDefer.reject(error);
-    });
+    .fail(requestDefer.reject);
   var pipedPromise = requestPromise.then(
     null,
     exports.handleRequestError.bind(null, hoodie)