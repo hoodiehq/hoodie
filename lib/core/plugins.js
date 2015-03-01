hoodiehq/hoodie-server

feat(plugins): expose hoodie-server environment to plugins
Plugins can now take full advantage of all hoodie-server internal
environment values, like the application’s path on the filesystem,
or CouchDB’s raw port number etc.

* * *

This commit was sponsored by The Hoodie Firm.
You can hire The Hoodie Firm:
http://go.hood.ie/thehoodiefirm

lib/core/plugins.js
@@ -87,6 +87,7 @@ exports.startPlugin = function (name, cfg, hoodie, callback) {
     return cb();
   };
 
+  hoodie.env = cfg;
   async.applyEachSeries([
     doStartPlugin,
     doInitHooks

hoodiehq/hoodie

lib/core/plugins.js
@@ -87,6 +87,7 @@ exports.startPlugin = function (name, env_config, hoodie, callback) {
     return cb();
   };
 
+  hoodie.env = env_config;
   async.applyEachSeries([
     doStartPlugin,
     doInitHooks