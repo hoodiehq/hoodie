hoodiehq/hoodie

commit aa8fc3a187ccbbb0c0acd028e924fcbc6a1f0664

fix: restart pull request correctly
* * *

This commit was sponsored by The Hoodie Firm.
You can hire The Hoodie Firm:

http://go.hood.ie/thehoodiefirm

src/lib/store/remote/api.js
@@ -161,7 +161,7 @@ exports.pull = function(state) {
 
   if (state.remote.isConnected()) {
     global.clearTimeout(state.pullRequestTimeout);
-    state.pullRequestTimeout = global.setTimeout(state.restartPullRequest, 25000);
+    state.pullRequestTimeout = global.setTimeout(helpers.restartPullRequest, 25000);
   }
 
   return state.pullRequest

commit 4f3d48ca35d5e4f946eae738eaa1f7c928ebea79

fix: pass state when restarting pull request
fixes bug introduced in aa8fc3a

* * *

This commit was sponsored by The Hoodie Firm.
You can hire The Hoodie Firm:

http://go.hood.ie/thehoodiefirm

src/lib/store/remote/api.js
@@ -174,7 +174,7 @@ exports.pull = function(state, options) {
 
   if (state.remote.isConnected()) {
     global.clearTimeout(state.pullRequestTimeout);
-    state.pullRequestTimeout = global.setTimeout(helpers.restartPullRequest, 25000);
+    state.pullRequestTimeout = global.setTimeout(helpers.restartPullRequest.bind(null, state), 25000);
   }
 
   return state.pullRequest