b6acf8d5191a106340621fd6c109c7a296c057d4
Stephan Bönnemann <stephan@boennemann.me>
2015-08-10 20:43:12 +0200

    revert: Check for a document id on document creation
    This reverts commit https://github.com/hoodiehq/hoodie-plugins-api/commit/9666eacf70eab70e548dc645c950661eace77591
    This happended when hoodie-plugins-api was it's own repository.
    
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    
    http://go.hood.ie/thehoodiefirm
    
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    
    http://go.hood.ie/thehoodiefirm

--- test/integration/plugins/api/test-plugin-api.js
+++ test/integration/plugins/api/test-plugin-api.js
@@ -468,22 +468,22 @@ exports['db.add / db.findAll / db.removeAll / db.findAll'] = function (test) {
   })
 }
 
-exports['reject a document with no id'] = function (test) {
-  var hoodie = new PluginAPI(DEFAULT_OPTIONS)
-  hoodie.database.add('foo', function (err, db) {
-    var doc = { title: 'Test' }
-
-    if (err) {
-      return test.done(err)
-    }
-
-    db.add('mytype', doc, function (err, results) {
-      test.ok(err)
-      test.strictEqual(results, undefined)
-      return test.done()
-    })
-  })
-}
+// exports['reject a document with no id'] = function (test) {
+//   var hoodie = new PluginAPI(DEFAULT_OPTIONS)
+//   hoodie.database.add('foo', function (err, db) {
+//     var doc = { title: 'Test' }
+//
+//     if (err) {
+//       return test.done(err)
+//     }
+//
+//     db.add('mytype', doc, function (err, results) {
+//       test.ok(err)
+//       test.strictEqual(results, undefined)
+//       return test.done()
+//     })
+//   })
+// }
 
 exports['config.set / config.get'] = function (test) {
   var hoodie = new PluginAPI(DEFAULT_OPTIONS)
