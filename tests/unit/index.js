594c925a9c9e9466551e7d3c1d6171ce2f633db2
Gregor Martynus <gregor@martynus.net>
2015-08-21 11:54:12 -0400

    test(walktrough): initial version

--- tests/unit/index.js
+++ tests/unit/index.js
@@ -5,6 +5,7 @@ var test = require('tape').createHarness()
 
 require('./api.js')(test)
 require('./hoodie.js')(test)
+// require('./walktrough.js')(test)
 
 var reqOptions = url.parse(process.env.TEST_RESULT_SERVER)
 reqOptions.method = 'POST'
