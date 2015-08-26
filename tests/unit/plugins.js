d7a0d9ca66f211f0b7e563936cc34e96dbebea21
Christoph Witzko <github@christophwitzko.com>
2015-08-26 16:21:52 +0200

    test(plugins): added plugin hook tests

--- tests/unit/plugins.js
+++ tests/unit/plugins.js
@@ -4,9 +4,24 @@ module.exports = function (test) {
   test('plugins', function (t) {
     var hoodie = new Hoodie(window.location.origin)
     t.is(typeof hoodie.test, 'function', 'method exposed')
-    hoodie.test({foo: 'bar'})
+    hoodie.request('get', hoodie.baseUrl + '/test/route')
     .then(function (res) {
-      t.is(res.foo, 'bar', 'response')
+      t.ok(res.ok, 'GET test route')
+
+      return hoodie.request('GET', '/_plugins/test/_api')
+    }, t.fail)
+    .then(function (res) {
+      t.is(res.method, 'get', 'dynamic hook GET')
+
+      return hoodie.request('POST', '/_plugins/test/_api', {data: {test: true}})
+    }, t.fail)
+    .then(function (res) {
+      t.ok(res.payload.test, 'dynamic hook POST')
+
+      return hoodie.test({foo: 'bar'})
+    }, t.fail)
+    .then(function (res) {
+      t.is(res.foo, 'bar', 'response is ok')
       t.end()
     }, t.fail)
   })
