e2902f9d50c85230fb48f6156293a49aa42c1646
Gregor <gregor@martynus.net>
2016-05-01 14:08:20 -0400

    WIP – config unit tests

--- test/unit/config-test.js
+++ test/unit/config-test.js
@@ -9,22 +9,22 @@ test('config', function (t) {
   t.test('default', function (tt) {
     var getConfig = require('../../lib/config')
 
-    var config = getConfig({})
+    getConfig({}, function (error, config) {
+      tt.error(error)
 
-    tt.is(config.name, 'hoodie', 'exposes name from package.json')
-    tt.ok(config.paths.data.startsWith(cwd), 'derives hoodie path from cwd')
-    tt.match(config.paths.public, cwd + '/public', 'falls back to hoodie/public')
+      tt.is(config.name, 'hoodie', 'exposes name from package.json')
+      tt.ok(config.paths.data.startsWith(cwd), 'derives hoodie path from cwd')
+      tt.match(config.paths.public, cwd + '/public', 'falls back to hoodie/public')
 
-    tt.same(config.db, {
-      prefix: cwd + '/.hoodie/data/'
-    }, 'uses default db config')
-    tt.same(config.app, {
-      hostname: '127.0.0.1',
-      port: 8080,
-      protocol: 'http'
-    }, 'uses "http://127.0.0.1:8080/" as app url')
+      tt.same(config.db.prefix, cwd + '/.hoodie/data/', 'uses default db config')
+      tt.same(config.app, {
+        hostname: '127.0.0.1',
+        port: 8080,
+        protocol: 'http'
+      }, 'uses "http://127.0.0.1:8080/" as app url')
 
-    tt.end()
+      tt.end()
+    })
   })
 
   t.test('applies overwrites', function (tt) {
@@ -41,53 +41,70 @@ test('config', function (t) {
         protocol: 'http'
       },
       db: {},
-      admin: {}
+      account: {},
+      admin: {},
+      store: {}
     })
 
-    var config = getConfig({
+    simple.mock(getConfig.internals, 'pouchDbConfig').callbackWith(null, {
+      secret: 'secret',
+      authentication_db: '_users',
+      admins: {}
+    })
+
+    getConfig({
       data: 'data-path',
       public: 'public-path',
       bindAddress: 'hoodie-test',
       port: 1337
+    }, function (error, config) {
+      tt.error(error)
+
+      tt.is(config.name, 'overwritten', 'exposes name from package.json')
+      tt.is(config.paths.data, 'data-path', 'uses data option as data path')
+      tt.is(config.paths.public, 'public-path', 'uses public option as public path')
+
+      tt.same(config.app, {
+        hostname: 'hoodie-test',
+        port: 1337,
+        protocol: 'http'
+      }, 'uses "http://hoodie-test:1337/" as app url')
+
+      simple.restore()
+      tt.end()
     })
-
-    tt.is(config.name, 'overwritten', 'exposes name from package.json')
-    tt.is(config.paths.data, 'data-path', 'uses data option as data path')
-    tt.is(config.paths.public, 'public-path', 'uses public option as public path')
-
-    tt.same(config.app, {
-      hostname: 'hoodie-test',
-      port: 1337,
-      protocol: 'http'
-    }, 'uses "http://hoodie-test:1337/" as app url')
-
-    simple.restore()
-    tt.end()
   })
 
   t.test('custom db', function (tt) {
-    tt.plan(2)
+    tt.plan(3)
 
     var memdown = {}
-
     var getConfig = proxyquire('../../lib/config', {
       memdown: memdown,
       npmlog: {
-        warn: function () {
-          tt.ok(true, 'warns about missing auth in db url')
-        },
+        warn: function () {},
         info: function () {}
       }
     })
 
-    var config = getConfig({
-      inMemory: true
+    simple.mock(getConfig.internals, 'couchDbConfig').callbackWith(null, {
+      secret: 'secret',
+      authentication_db: '_users',
+      admins: {}
     })
 
-    tt.is(config.db.db, memdown, 'uses memdown for in memory')
+    getConfig({
+      inMemory: true
+    }, function (error, config) {
+      tt.error(error)
+      tt.is(config.db.db, memdown, 'uses memdown for in memory')
+    })
 
     getConfig({
       dbUrl: 'http://example.com/'
+    }, function (error, config) {
+      simple.restore()
+      tt.ok(error, 'returns error if db URL misses auth')
     })
   })
 
