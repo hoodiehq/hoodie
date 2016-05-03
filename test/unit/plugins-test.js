d527e0e30f57eeed12566722892e546df927c925
Gregor <gregor@martynus.net>
2016-05-02 20:45:07 -0400

    WIP - refactor plugin registration

--- test/unit/plugins-test.js
+++ test/unit/plugins-test.js
@@ -1,24 +1,24 @@
+var simple = require('simple-mock')
 var test = require('tap').test
-require('npmlog').level = 'silent'
-
-var pluginMock = function () {}
-pluginMock['@noCallThru'] = true
-var proxyquire = require('proxyquire', {
-  '@hoodie/store': pluginMock,
-  '@hoodie/account': pluginMock
-})
+var proxyquire = require('proxyquire').noCallThru()
 
 var registerPlugins = proxyquire('../../lib/plugins', {
-
+  '@hoodie/store': function () {},
+  '@hoodie/account': function () {}
 })
+
+require('npmlog').level = 'silent'
 
 test('plugins', function (group) {
   group.test('account registration', function (group) {
-    registerPlugins({
-      account: 'account options'
-    }, function (error, plugins) {
+    var server = {
+      register: simple.stub().callbackWith(null)
+    }
+    var config = { account: 'account options' }
+    registerPlugins(server, config, function (error) {
       group.error(error)
 
+      var plugins = server.register.lastCall.arg
       var accountPlugin = plugins[plugins.length - 2]
 
       group.is(accountPlugin.options, 'account options', 'passes config.account')
@@ -29,11 +29,14 @@ test('plugins', function (group) {
   })
 
   group.test('store registration', function (group) {
-    registerPlugins({
-      store: 'store options'
-    }, function (error, plugins) {
+    var server = {
+      register: simple.stub().callbackWith(null)
+    }
+    var config = { store: 'store options' }
+    registerPlugins(server, config, function (error) {
       group.error(error)
 
+      var plugins = server.register.lastCall.arg
       var accountPlugin = plugins[plugins.length - 1]
 
       group.is(accountPlugin.options, 'store options', 'passes config.store')
