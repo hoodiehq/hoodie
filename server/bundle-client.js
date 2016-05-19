af1d0240e78991976d9e975db2a5cb8b09b14527
James West <james@jameswest.co.nz>
2016-05-20 10:16:11 +1200

    Reverted bundle-client.js code to move to another branch

--- server/bundle-client.js
+++ server/bundle-client.js
@@ -4,65 +4,31 @@ var fs = require('fs')
 var path = require('path')
 
 var log = require('npmlog')
-var browserify = require('browserify')
-var async = require('async')
 
 function bundleClient (config, callback) {
   var hoodieClientModulePath = path.dirname(require.resolve('@hoodie/client/package.json'))
-  var hoodieClientPath = path.join(hoodieClientModulePath, 'index.js')
+  var hoodieClientPath = path.join(hoodieClientModulePath, 'dist/hoodie.js')
   var bundleTargetPath = path.join(config.paths.data, 'client.js')
-  var bundleTargetMinPath = path.join(config.paths.data, 'client.min.js')
 
-  var bundler = browserify()
-  bundler.add(path.join(hoodieClientModulePath, 'index.js'))
-  Object.keys(config.plugins).map(function (key) {
-    var plugin = config.plugins[key]
-    // check we have a client file in place
-    try {
-      require.resolve(plugin.package + '/client/index.js')
-    } catch (e) {
-      return false
-    }
-    var pluginPath = path.dirname(require.resolve(plugin.package + '/package.json'))
-    bundler.add(path.join(pluginPath, 'client/index.js'))
-  })
+  // https://github.com/hoodiehq/hoodie-client/issues/34
+  // var hoodieMinPath = path.join(hoodieClientModulePath, 'dist/hoodie.min.js')
 
-  // output client
   log.silly('bundle', 'bundling ' + hoodieClientPath + ' into ' + bundleTargetPath)
 
-  var bundleFs = fs.createWriteStream(bundleTargetPath)
-  var bundleFsMin = fs.createWriteStream(bundleTargetMinPath)
+  var stream = fs.createReadStream(hoodieClientPath)
+  stream.pipe(fs.createWriteStream(bundleTargetPath))
+  stream.on('error', callback)
+  stream.on('end', function () {
+    // TODO: pass client configuration to constructor
+    fs.appendFile(bundleTargetPath, '\n\nhoodie = new Hoodie()', function (error) {
+      if (error) {
+        return callback(error)
+      }
 
-  async.series([
-    function (callback) {
-      bundleFs.on('finish', function () {
-        fs.appendFile(bundleTargetPath, '\n\nhoodie = new Hoodie()', function (error) {
-          if (error) return new Error(error)
-          log.info('bundle', 'bundled Hoodie client into ' + bundleTargetPath)
-          callback()
-        })
-      })
-    },
-    function (callback) {
-      bundleFsMin.on('finish', function () {
-        fs.appendFile(bundleTargetMinPath, '\n\nhoodie = new Hoodie()', function (error) {
-          if (error) return new Error(error)
-          log.info('bundle', 'bundled Hoodie client into ' + bundleTargetMinPath)
-          callback()
-        })
-      })
-    }
-  ],
-  function (err, results) {
-    if (err) return callback(err)
-    log.info('bundle', 'done')
-    callback()
+      log.silly('bundle', 'appended Hoodie init code to ' + bundleTargetPath)
+      log.info('bundle', 'bundled Hoodie client into ' + bundleTargetPath)
+
+      callback()
+    })
   })
-
-  // process
-  bundler.bundle().pipe(bundleFs)
-  // and minify
-  bundler.transform({
-    global: true
-  }, 'uglifyify').bundle().pipe(bundleFsMin)
 }
