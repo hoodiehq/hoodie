d43e084a3953aac7d32726941f4fb71e98150813
Olu Niyi-Awosusi <o.niyiawosusi@gmail.com>
2016-06-07 21:26:41 +0100

    added first test for user database remove

--- test/unit/utils-test.js
+++ test/unit/utils-test.js
@@ -2,13 +2,23 @@ var test = require('tap').test
 require('npmlog').level = 'silent'
 
 var removeAuth = require('../../server/utils/remove-auth-from-url')
+var userDatabases = require('../../server/utils/user-databases')
 
 test('utils', function (group) {
-  group.test('removeAuth', function (group) {
-    group.is(removeAuth('http://foo:secret@example.com:1234'), 'http://example.com:1234', 'removes username & password from url')
-    group.is(removeAuth('http://example.com'), 'http://example.com', 'returns same URL if no username or password passed')
+  group.test('removeAuth', function (t) {
+    t.is(removeAuth('http://foo:secret@example.com:1234'), 'http://example.com:1234', 'removes username & password from url')
+    t.is(removeAuth('http://example.com'), 'http://example.com', 'returns same URL if no username or password passed')
 
-    group.end()
+    t.end()
+  })
+
+  group.test('user-databases.remove', function (t) {
+    var config = {}
+    config.db = {}
+    config.db.url = false
+    userDatabases.remove(config, {}, {})
+
+    t.end()
   })
 
   group.end()
