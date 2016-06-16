1229f4467529b2257fea1f090de96d8917b9b4e3
Gregor <gregor@martynus.net>
2016-06-15 23:51:56 -0400

    feat: write client bundle for caching (unless in memory)

--- server/plugins/client/index.js
+++ server/plugins/client/index.js
@@ -32,6 +32,27 @@ function register (server, options, next) {
             }
 
             resolve(bundleBuffer)
+
+            if (!hasUpdate) {
+              log.info('client', 'bundle is up to date')
+              return
+            }
+
+            if (options.config.inMemory) {
+              log.silly('client', 'running in memory, not writing bundle to ' + bundleTargetPath)
+              return
+            }
+
+            log.info('client', 'bundle is out of date')
+            log.silly('client', 'writing bundle to ' + bundleTargetPath)
+            fs.writeFile(bundleTargetPath, bundleBuffer, function (error) {
+              if (error) {
+                log.warn('client', 'could not write to ' + bundleTargetPath + '. Bundle cannot be cached and will be re-generated on server restart')
+                return
+              }
+
+              log.info('client', 'bundle written to ' + bundleTargetPath)
+            })
           })
         })
       }
