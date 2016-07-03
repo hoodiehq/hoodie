a66d8d4584f1681206cab28dbd68a996dc367cde
Gregor <gregor@martynus.net>
2016-07-03 16:05:51 -0400

    refactor: rename `group` to `t` where applicable

--- test/unit/config/parse-options-test.js
+++ test/unit/config/parse-options-test.js
@@ -32,20 +32,20 @@ var parseOptions = proxyquire('../../../server/config/parse-options', {
 })
 
 test('parse options', function (group) {
-  group.test('defaults', function (group) {
+  group.test('defaults', function (t) {
     var config = parseOptions({})
 
-    group.is(config.name, 'foo', 'sets config.name from defaults')
-    group.is(config.paths.data, 'data path', 'sets config.paths.data from defaults')
-    group.is(config.paths.public, 'public path', 'sets config.public.data from defaults')
-    group.is(config.connection.host, 'host name', 'sets config.connection.host from defaults')
-    group.is(config.connection.port, 'app port', 'sets config.connection.port from defaults')
-    group.is(config.db.prefix, 'data path/data' + pathSeperator, 'sets config.db.prefix based on default data path')
+    t.is(config.name, 'foo', 'sets config.name from defaults')
+    t.is(config.paths.data, 'data path', 'sets config.paths.data from defaults')
+    t.is(config.paths.public, 'public path', 'sets config.public.data from defaults')
+    t.is(config.connection.host, 'host name', 'sets config.connection.host from defaults')
+    t.is(config.connection.port, 'app port', 'sets config.connection.port from defaults')
+    t.is(config.db.prefix, 'data path/data' + pathSeperator, 'sets config.db.prefix based on default data path')
 
-    group.end()
+    t.end()
   })
 
-  group.test('overwrites', function (group) {
+  group.test('overwrites', function (t) {
     var config = parseOptions({
       data: 'options.data',
       public: 'options.public',
@@ -53,32 +53,32 @@ test('parse options', function (group) {
       port: 'options.port'
     })
 
-    group.is(config.paths.data, 'options.data', 'uses data option as data path')
-    group.is(config.paths.public, 'options.public', 'uses public option as public path')
-    group.is(config.connection.host, 'options.bindAddress', 'sets config.connection.host from options.bindAddress')
-    group.is(config.connection.port, 'options.port', 'sets config.connection.port from options.port')
+    t.is(config.paths.data, 'options.data', 'uses data option as data path')
+    t.is(config.paths.public, 'options.public', 'uses public option as public path')
+    t.is(config.connection.host, 'options.bindAddress', 'sets config.connection.host from options.bindAddress')
+    t.is(config.connection.port, 'options.port', 'sets config.connection.port from options.port')
 
-    group.end()
+    t.end()
   })
 
-  group.test('dbUrl', function (group) {
+  group.test('dbUrl', function (t) {
     var config = parseOptions({
       dbUrl: 'http://foo:bar@baz.com'
     })
 
-    group.is(config.db.url, 'http://foo:bar@baz.com', 'Sets config.db.url')
+    t.is(config.db.url, 'http://foo:bar@baz.com', 'Sets config.db.url')
 
-    group.end()
+    t.end()
   })
 
-  group.test('inMemory', function (group) {
+  group.test('inMemory', function (t) {
     var config = parseOptions({
       inMemory: true
     })
 
-    group.is(config.db.db, memdownMock, 'Sets config.db.db to memdown')
+    t.is(config.db.db, memdownMock, 'Sets config.db.db to memdown')
 
-    group.end()
+    t.end()
   })
 
   group.end()
