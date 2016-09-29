3101c74722d9961907a795b0a952f1cacce546df
Gabriel Perales <gabrielperales@gmail.com>
2016-09-30 00:27:59 +0100

    feat: npm start -- console
    Added npmlog to loggin error messages and removed logic for find a free port

--- server/plugins/repl.js
+++ server/plugins/repl.js
@@ -3,40 +3,29 @@ module.exports.register.attributes = {
   name: 'hoodie-repl'
 }
 
-var Net = require('net')
+var log = require('npmlog')
 var AdminClient = require('@hoodie/admin-client')
-
-var availablePort = function (callback) {
-  var server = Net.createServer()
-  server.listen(0, () => {
-    var port = server.address().port
-    server.close(() => {
-      callback(port)
-    })
-  })
-}
 
 function register (server, options, next) {
   if (process.argv[2] !== 'console') return next()
 
-  availablePort(function (portAvailable) {
-    var port = options.replPort || portAvailable
+  var port = options.replPort || 9000
 
-    console.log('Hoodie REPL available on port ' + port)
+  console.log('Hoodie REPL available on port ' + port)
+  console.log('Type \'.exit\' in the REPL to exit')
 
-    server.register({
-      register: require('reptile'),
-      options: {
-        context: new AdminClient(),
-        port: port,
-        replOptions: {
-          prompt: 'hoodie > '
-        }
+  server.register({
+    register: require('reptile'),
+    options: {
+      context: new AdminClient(),
+      port: port,
+      replOptions: {
+        prompt: 'hoodie > '
       }
-    }, function (error) {
-      if (error) console.log(error)
-      return next()
-    })
+    }
+  }, function (error) {
+    if (error) log.error('repl', error)
+    return next()
   })
 }
 
