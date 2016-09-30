98da7aa9a1274b727f4ee256e457e0c0c5f0a736
Gabriel Perales <gabrielperales@gmail.com>
2016-09-30 23:25:29 +0100

    added test to check if the admin client is in the REPL scope

--- test/integration/repl-test.js
+++ test/integration/repl-test.js
@@ -20,14 +20,14 @@ var hapiPluginOptions = {
 
 require('npmlog').level = 'error'
 
-test('connect to the REPL server', function (t) {
-  t.plan(2)
+test('admin client in the REPL scope', function (t) {
   var server = new Hapi.Server(hapiOptions)
-  server.connection({port: 8090})
-  server.register(hapiPluginOptions, function (error) {
-    t.notOk(error)
 
+  server.connection({port: 8090})
+
+  server.register(hapiPluginOptions, function () {
     var address = Net.Socket.prototype.address
+
     Net.Socket.prototype.address = function () {
       Net.Socket.prototype.address = address
       return {
@@ -39,10 +39,12 @@ test('connect to the REPL server', function (t) {
     var sock = Net.connect(9000)
 
     sock.once('close', function () {
-      t.pass()
+      t.end()
       // TODO I couldn't find another way to stop the REPL server
       process.exit(0)
     })
+
+    var state = 0
 
     sock.on('readable', function (size) {
       var buffer = sock.read()
@@ -50,7 +52,21 @@ test('connect to the REPL server', function (t) {
         return
       }
 
-      sock.write('.exit\n')
+      var result = buffer.toString('ascii')
+
+      if (state === 0) {
+        t.notEqual(result.indexOf('hoodie >'), -1)
+        sock.write('accounts\n')
+      } else if (state === 1) {
+        t.notEqual(result.indexOf('add'), -1)
+        t.notEqual(result.indexOf('find'), -1)
+        t.notEqual(result.indexOf('findAll'), -1)
+        t.notEqual(result.indexOf('update'), -1)
+        t.notEqual(result.indexOf('remove'), -1)
+        sock.write('.exit\n')
+      }
+
+      state++
     })
   })
 })
