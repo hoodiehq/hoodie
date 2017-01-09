fa356981e4056fb111b3d7ca3c8e9c5439f921d2
Benjamin R. White <ben@delt.as>
2017-01-09 12:30:54 +0000

    test: add script for ensuring quick-start instructions function correctly

---
+++ bin/quick-start-test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+HOODIE_FOLDER=$(pwd)
+TEMP_ROOT=$(mktemp -d)
+pushd $TEMP_ROOT
+trap "{ popd; rm -rf $TEMP_ROOT; exit 255; }" EXIT
+
+## docs/guides/quickstart.rst:41
+
+npm init -y
+
+## docs/guides/quickstart.rst:47
+
+npm install $HOODIE_FOLDER --save
+
+## docs/guides/quickstart.rst:72
+
+npm start & HOODIE_PROCESS=$!
+
+## Give the process a generous sixty seconds to start
+
+RETRIES=60
+while [ "$RETRIES" -gt 0 ] ; do
+  sleep 1
+  kill -0 $HOODIE_PROCESS 2&>1 > /dev/null || { echo "Hoodie exited prematurely"; wait $HOODIE_PROCESS ; exit $? ; }
+  curl -I http://localhost:8080/hoodie/client.js > /dev/null && break
+  let RETRIES=RETRIES-1
+done
+
+kill $HOODIE_PROCESS
+
+[ $RETRIES -le 0 ] && { echo "Timed out waiting for hoodie to start"; exit 1 ; }
+
+exit 0;
