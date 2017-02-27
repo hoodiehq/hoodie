01481c65822eadb21f0293889b0c1733c4191f76
Tim <fozy81@hotmail.com>
2017-02-27 21:12:16 +0000

    fix: only need rtd_theme
    checking to see if this is causing build failure on readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -75,21 +75,21 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = ''
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
+#if not on_rtd:  # only import and set the theme if we're building docs locally
+ #   import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+  #  html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 #  Custom Hoodie theming?
 #    html_style = 'css/hoodie.css'
-else:
-    html_context = {
-        'css_files': [
-            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
-            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+#else:
+ #   html_context = {
+  #      'css_files': [
+   #         'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+    #        'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
 #  Custom Hoodie theming?
 #            '_static/css/hoodie.css',
-        ],
-    }
+     #   ],
+    #}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
