25cd5be9f55f45640961a845f1b9ae0d16690034
Tim <fozy81@hotmail.com>
2017-02-27 21:20:38 +0000

    fix: remove link to image
    image currently not available.

--- docs/conf.py
+++ docs/conf.py
@@ -108,7 +108,7 @@ pygments_style = 'sphinx'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'low-profile-dog-1.png'
+#html_logo = 'low-profile-dog-1.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
