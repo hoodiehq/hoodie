81480ab38f6174f5fe169bcdeb61c00da9f281e8
Alice Anjali <alice.anjali1995@gmail.com>
2017-03-14 06:26:07 +0530

    test: 💯 Code Coverage (#722)

--- test/unit/server/loader-test.js
+++ test/unit/server/loader-test.js
@@ -6,8 +6,9 @@ var mockResolver = simple.stub()
 var loader = proxyquire('../../../server/plugins', {
   './resolver': mockResolver
 })
+var registerPluginsError = new Error('Plugin Register Error')
 var serverMock = {
-  register: simple.stub().callbackWith(null)
+  register: simple.stub().callbackWith(registerPluginsError)
 }
 
 test('when require.resolve errors', function (t) {
@@ -26,4 +27,20 @@ test('when require.resolve errors', function (t) {
     })
   }, error, 'the error is rethrown')
   t.end()
+})
+
+test('when registerPlugins errors', function (t) {
+  var options = {
+    paths: {
+      public: 'public'
+    }
+  }
+  var error = new Error('Module Not Found Error')
+  error.code = 'MODULE_NOT_FOUND'
+  mockResolver.throwWith(error)
+  loader(serverMock, options, function (error) {
+    t.ok(error)
+    t.equal(error, registerPluginsError)
+    t.end()
+  })
 })
